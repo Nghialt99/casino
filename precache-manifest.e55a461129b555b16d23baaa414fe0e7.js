c90e27ef78e47721c0d12bf573bba70cf7c4aa42
MY_PC <MY_PC@DESKTOP-E28TGCN>
2020-02-06 13:32:04 +0700

    Updates

---
+++ precache-manifest.e55a461129b555b16d23baaa414fe0e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d2a1ddcd0f5f6a17be8680bbdcf67b",
+    "url": "/nghialt99.github.io/index.html"
+  },
+  {
+    "revision": "30166422c79fc4377837",
+    "url": "/nghialt99.github.io/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0085382bfd862bd0523f",
+    "url": "/nghialt99.github.io/static/js/2.3efdfe61.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/nghialt99.github.io/static/js/2.3efdfe61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30166422c79fc4377837",
+    "url": "/nghialt99.github.io/static/js/main.69d4ca55.chunk.js"
+  },
+  {
+    "revision": "2a24d141255c3964d51d",
+    "url": "/nghialt99.github.io/static/js/runtime-main.c3d9a697.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nghialt99.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
